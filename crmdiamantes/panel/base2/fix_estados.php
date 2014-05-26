d0ccac14721c985eab700becf65ab9f661d1d32c
guillermolozan <guillermolozan@gmail.com>
2014-05-26 07:57:45 -0500

    ...

---
+++ crmdiamantes/panel/base2/fix_estados.php
@@ -0,0 +1,7 @@
+﻿<?php  
+if($_GET['i']!=''){
+	$venta=fila("id_item_item","productos_ventas","where id=".$_GET['i']." and saldo=0");
+	if($venta['id_item_item']){
+		update(array('estado'=>'4'),"productos_items_items","where id='".$venta['id_item_item']."'");
+	}
+}
